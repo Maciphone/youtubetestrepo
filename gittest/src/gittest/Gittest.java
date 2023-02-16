9f0c168ca681ede8671d639befc47eda219a5645
Maciphone <mackokuckoja@gmail.com>
2023-02-16 10:14:23 +0100

    most el is mentettem a váltorzást

--- gittest/src/gittest/Gittest.java
+++ gittest/src/gittest/Gittest.java
@@ -4,14 +4,15 @@ package gittest;
  * @author --G--
  */
 public class Gittest {
-
+    
     public static void main(String[] args) {
         gittest("hate git");
-
+        gittest("rohadtul utálom a githubot");
+        
     }
-
+    
     private static void gittest(String hate_git) {
         System.out.println(hate_git);
     }
-
+    
 }
