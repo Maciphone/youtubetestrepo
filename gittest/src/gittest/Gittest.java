a402998b90435432e3fad134d36b0ec8145be87c
Maciphone <mackokuckoja@gmail.com>
2023-02-16 11:18:39 +0100

    test new branchbe

--- gittest/src/gittest/Gittest.java
+++ gittest/src/gittest/Gittest.java
@@ -4,17 +4,18 @@ package gittest;
  * @author --G--
  */
 public class Gittest {
-    
+
     public static void main(String[] args) {
         gittest("hate git");
         gittest("rohadtul utálom a githubot");
         gittest("lehet csak gyűlölöm, mert nem értem még");
         gittest("git push --set-upstream origin new_master");
-        
+        gittest("test_new_masterbe");
+
     }
-    
+
     private static void gittest(String hate_git) {
         System.out.println(hate_git);
     }
-    
+
 }
