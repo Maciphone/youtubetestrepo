1848c849586388d6dabdb07f4f4560d4f5e586ff
Maciphone <mackokuckoja@gmail.com>
2023-02-16 10:48:38 +0100

    elvesztem

--- gittest/src/gittest/Gittest.java
+++ gittest/src/gittest/Gittest.java
@@ -4,16 +4,17 @@ package gittest;
  * @author --G--
  */
 public class Gittest {
-
+    
     public static void main(String[] args) {
         gittest("hate git");
         gittest("rohadtul utálom a githubot");
         gittest("lehet csak gyűlölöm, mert nem értem még");
-
+        gittest("git push --set-upstream origin new_master");
+        
     }
-
+    
     private static void gittest(String hate_git) {
         System.out.println(hate_git);
     }
-
+    
 }
