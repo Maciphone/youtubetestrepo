7956d83378db1b90d3233250c00510631a68f429
Maciphone <mackokuckoja@gmail.com>
2023-02-16 10:22:36 +0100

    lehet csak gyűlölöm, mert nem értem még

--- gittest/src/gittest/Gittest.java
+++ gittest/src/gittest/Gittest.java
@@ -4,15 +4,16 @@ package gittest;
  * @author --G--
  */
 public class Gittest {
-    
+
     public static void main(String[] args) {
         gittest("hate git");
         gittest("rohadtul utálom a githubot");
-        
+        gittest("lehet csak gyűlölöm, mert nem értem még");
+
     }
-    
+
     private static void gittest(String hate_git) {
         System.out.println(hate_git);
     }
-    
+
 }
