5cc008e888373429b0b37d516cf802e0a9d23159
Maciphone <mackokuckoja@gmail.com>
2023-02-16 10:06:15 +0100

    initial commit, létrehoztam a java filet a repositoryba

---
+++ gittest/src/gittest/Gittest.java
@@ -0,0 +1,17 @@
+package gittest;
+
+/**
+ * @author --G--
+ */
+public class Gittest {
+
+    public static void main(String[] args) {
+        gittest("hate git");
+
+    }
+
+    private static void gittest(String hate_git) {
+        System.out.println(hate_git);
+    }
+
+}
